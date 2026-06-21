d5013ac515475c9f890ffd9bdadf440882839b0d
Fauzan Fauzia Fauzi <fauzanfauziafauzi@gmail.com>
2026-06-22 01:38:46 +0700

    Update dml_products.sql: Update prices, categories, descriptions, and cleanup data

--- dml_products.sql
+++ dml_products.sql
@@ -28,3 +28,29 @@ SELECT id, Name FROM Products WHERE Name = 'Pasta Carbonara';
 SELECT id, Name, Description, Price FROM Products WHERE Price = 45000;
 SELECT id, Name, Description, Price FROM Products WHERE Price < 35000;
 
+-- Update data satu per satu
+UPDATE Products SET Category = 'Makanan' WHERE id = 'P0001';
+
+UPDATE Products SET 
+    Category = 'Makanan', 
+    Description = 'Pasta + Parmesan + Telur + Beef' 
+WHERE id = 'P0003';
+
+UPDATE Products SET Price = 65000 WHERE id = 'P0004';
+
+UPDATE Products SET Price = Price + 30000 WHERE id = 'P0005';
+
+-- Insert dan Delete
+INSERT INTO Products (id, Name, Price, Quantity) VALUES ('P0006', 'Pizza Full Cheese', 55000, 12);
+DELETE FROM Products WHERE id = 'P0006';
+
+-- Update massal atau update field lain
+UPDATE Products SET Category = 'Makanan', Description = 'Pasta dan Saus Daging' WHERE id = 'P0005';
+UPDATE Products SET Category = 'Makanan', Description = 'Mie Ayam Dengan Topping Ayam Kecap Gurih' WHERE id = 'P0001';
+UPDATE Products SET Category = 'Makanan' WHERE id = 'P0002';
+UPDATE Products SET Category = 'Makanan', Description = 'Pasta dengan bawang putih dan olive oil' WHERE id = 'P0004';
+UPDATE Products SET Price = 20000 WHERE id = 'P0001';
+
+-- Cek hasil akhir
+SELECT * FROM Products;
+
