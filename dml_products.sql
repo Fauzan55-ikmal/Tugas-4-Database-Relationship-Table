8a6aa2523773ccbcf57e57f9383af8312a6f0481
Fauzan Fauzia Fauzi <fauzanfauziafauzi@gmail.com>
2026-06-22 01:31:31 +0700

    Add dml_products.sql: Insert initial product data

---
+++ dml_products.sql
@@ -0,0 +1,8 @@
+USE D112521003_eecommercee;
+
+-- Memasukkan data baru
+INSERT INTO Products (id, Name, Price, Quantity)
+VALUES ('P0001', 'Mie Ayam Original', 15000, 100);
+
+-- Menampilkan data
+SELECT * FROM Products;
