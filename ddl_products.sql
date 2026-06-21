938be60fe279eaf0d3767c786e6c53770ea7aebc
Fauzan Fauzia Fauzi <fauzanfauziafauzi@gmail.com>
2026-06-22 01:30:40 +0700

    Update ddl_products.sql: Rename table and modify columns

--- ddl_products.sql
+++ ddl_products.sql
@@ -1,7 +1,7 @@
 -- DDL untuk membuat dan memodifikasi tabel Barang
-USE D112521003_eecommerce;
+USE D112521003_eecommercee;
 
--- 1. Create awal
+-- Create awal
 CREATE TABLE Barang (
     Kode INT,
     Nama VARCHAR(100),
@@ -9,9 +9,25 @@ CREATE TABLE Barang (
     Jumlah INT
 );
 
--- 2. Histori modifikasi (ALTER)
+-- Histori modifikasi (ALTER)
 ALTER TABLE Barang ADD Nama_Coloumn TEXT;
 ALTER TABLE Barang DROP Nama;
 ALTER TABLE Barang CHANGE Nama_Coloumn Nama VARCHAR(100);
 ALTER TABLE Barang MODIFY Nama VARCHAR(100) AFTER Kode;
 ALTER TABLE Barang CHANGE Nama Nama VARCHAR(100) NOT NULL;
+
+-- Mengubah nama tabel
+RENAME TABLE Barang TO Products;
+
+-- Mengubah constraint kolom Nama
+ALTER TABLE Products CHANGE Nama Nama VARCHAR(100) NULL;
+
+-- Mengubah struktur tabel secara keseluruhan (Renaming, Adding, Modifying)
+ALTER TABLE Products
+    CHANGE COLUMN Kode id VARCHAR(10) NOT NULL,
+    CHANGE COLUMN Nama Name VARCHAR(100) NOT NULL,
+    ADD COLUMN Description TEXT AFTER Name,
+    CHANGE COLUMN Harga Price INT(10) UNSIGNED NOT NULL,
+    CHANGE COLUMN Jumlah Quantity INT(10) UNSIGNED NOT NULL DEFAULT 0,
+    ADD COLUMN Created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP();
+
